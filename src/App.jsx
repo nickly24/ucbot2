f2e7fc161a301818e922b42bafa68f05fda13f3c
nickly24 <nikolaypribysh@mail.ru>
2026-01-06 13:02:57 +0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import React, { useEffect } from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
-import { useTelegram } from './hooks/useTelegram'
-import Home from './pages/Home'
-import CodePurchase from './pages/CodePurchase'
-import CodeCart from './pages/CodeCart'
-import OrderHistory from './pages/OrderHistory'
-import MyCodes from './pages/MyCodes'
-import Instructions from './pages/Instructions'
+import React from 'react'
 import './App.css'
 
+// Временный простой рендер для проверки, что React вообще что‑то показывает в Telegram
 function App() {
-  const { initTelegram } = useTelegram()
-
-  useEffect(() => {
-    console.log('App component mounted')
-    initTelegram()
-  }, [initTelegram])
-
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/code-purchase" element={<CodePurchase />} />
-          <Route path="/code-cart" element={<CodeCart />} />
-          <Route path="/order-history" element={<OrderHistory />} />
-          <Route path="/my-codes" element={<MyCodes />} />
-          <Route path="/instructions" element={<Instructions />} />
-        </Routes>
-      </div>
-    </Router>
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'darkred',
+        color: 'white',
+        fontSize: '24px',
+        fontWeight: 'bold',
+      }}
+    >
+      Тестовый экран App.jsx
+    </div>
   )
 }
 
